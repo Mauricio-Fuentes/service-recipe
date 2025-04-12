a203e27414c1cb859975d45897271707c897a8f1
Mauricio Fuentes <m_fuentes_m@hotmail.com>
2025-04-11 21:27:13 -0400

    Change Coverage

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -19,4 +19,10 @@ describe('AppController', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
+
+  describe('root', () => {
+    it('should return "Service Up!"', () => {
+      expect(appController.getHealthcheck()).toBe('Service Up!');
+    });
+  });
 });
